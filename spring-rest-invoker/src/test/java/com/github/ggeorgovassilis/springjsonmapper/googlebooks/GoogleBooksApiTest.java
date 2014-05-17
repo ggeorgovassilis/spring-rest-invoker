3d24e75c4d691d917f6bed33c98567228ba6aa9a
ggeorgovassilis <g.georgovassilis@gmail.com>
2014-05-17 09:48:01 +0200

    Test more robust to changing result order

--- spring-rest-invoker/src/test/java/com/github/ggeorgovassilis/springjsonmapper/googlebooks/GoogleBooksApiTest.java
+++ spring-rest-invoker/src/test/java/com/github/ggeorgovassilis/springjsonmapper/googlebooks/GoogleBooksApiTest.java
@@ -12,28 +12,32 @@ import static org.junit.Assert.*;
  * Integration test with the google books API
  * @author george georgovassilis
  */
-@RunWith(value=SpringJUnit4ClassRunner.class)
+@RunWith(value = SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:test-context-googlebooks.xml")
 public class GoogleBooksApiTest {
 
-    @Autowired
-    BookService bookService;
+	@Autowired
+	BookService bookService;
 
-    @Test
-    public void testFindBooksByTitle(){
-	QueryResult result = bookService.findBooksByTitle("\"Philosophiae naturalis principia mathematica\"");
-	assertTrue(result.getItems().size()>0);
-	VolumeInfo info = result.getItems().get(0).getVolumeInfo();
-	assertEquals("Philosophiae naturalis principia mathematica", info.getTitle());
-	assertEquals("Isaac Newton", info.getAuthors().get(0));
-    }
+	@Test
+	public void testFindBooksByTitle() {
+		QueryResult result = bookService.findBooksByTitle("\"Philosophiae naturalis principia mathematica\"");
+		assertTrue(result.getItems().size() > 0);
+		boolean found = false;
+		for (Item item : result.getItems()) {
+			VolumeInfo info = item.getVolumeInfo();
+			found |= ("Philosophiae naturalis principia mathematica".equals(info.getTitle()) && "Isaac Newton".equals(info.getAuthors()
+					.get(0)));
+		}
+		assertTrue(found);
+	}
 
-    @Test
-    public void testFindBooksByid(){
-	Item item = bookService.findBookById("3h9_GY8v-hgC");
-	VolumeInfo info = item.getVolumeInfo();
-	assertEquals("Philosophiae naturalis principia mathematica", info.getTitle());
-	assertEquals("Isaac Newton", info.getAuthors().get(0));
-    }
+	@Test
+	public void testFindBooksByid() {
+		Item item = bookService.findBookById("3h9_GY8v-hgC");
+		VolumeInfo info = item.getVolumeInfo();
+		assertEquals("Philosophiae naturalis principia mathematica", info.getTitle());
+		assertEquals("Isaac Newton", info.getAuthors().get(0));
+	}
 
 }
