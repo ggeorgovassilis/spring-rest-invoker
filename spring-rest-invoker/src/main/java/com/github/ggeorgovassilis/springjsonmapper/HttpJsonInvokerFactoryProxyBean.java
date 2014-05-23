6a82a4b881f1ebfddb87c424852c6676188791a1
mguennec <maxime.guennec@gmail.com>
2014-05-23 08:51:49 -0400

    support for FormHttpMessageConverter

--- spring-rest-invoker/src/main/java/com/github/ggeorgovassilis/springjsonmapper/HttpJsonInvokerFactoryProxyBean.java
+++ spring-rest-invoker/src/main/java/com/github/ggeorgovassilis/springjsonmapper/HttpJsonInvokerFactoryProxyBean.java
@@ -207,7 +207,7 @@ public class HttpJsonInvokerFactoryProxyBean implements FactoryBean<Object>, Inv
 		String url = baseUrl;
 		Object result = null;
 		Map<String, Object> parameters = new HashMap<>();
-		Map<String, Object> dataObjects = new HashMap<>();
+		MultiValueMap<String, Object> dataObjects = new LinkedMultiValueMap<>();
 		RestOperations rest = getRestTemplate();
 		UrlMapping urlMapping = null;
 		RequestMapping requestMapping = getRequestMapping(method);
@@ -237,15 +237,14 @@ public class HttpJsonInvokerFactoryProxyBean implements FactoryBean<Object>, Inv
 			} else if (descriptor.getType().equals(Type.pathVariable)) {
 				url = url.replaceAll("\\{" + descriptor.getName() + "\\}", "" + descriptor.getValue());
 			} else if (descriptor.getType().equals(Type.requestBody)) {
-				dataObjects.put(descriptor.getName(), descriptor.getValue());
+				dataObjects.add(descriptor.getName(), descriptor.getValue());
 			}
 		}
 
 		if (RequestMethod.GET.equals(httpMethod)) {
 			result = rest.getForObject(url, method.getReturnType(), parameters);
 		} else {
-
-			Object dataObject = dataObjects.get("");
+			Object dataObject = dataObjects.getFirst("");
 			if (dataObjects.size() > 1 && dataObject != null)
 				throw new IllegalArgumentException(
 						"Found both named and anonymous @RequestBody arguments on method. Use either a single, anonymous, method parameter or annotate every @RequestBody parameter together with @RequestParam on "
